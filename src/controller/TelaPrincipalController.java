882b28b47ec6b8660879bc78076b84c8308fc4c1
victogr555 <victogr25@gmail.com>
2026-05-30 18:46:04 -0300

    arquivo telaPrincipal feita
    fiz a funcionalidade do botão para abrir as telas cadastro, partida e tabela. a funcionalidade trocar a tela

--- src/controller/TelaPrincipalController.java
+++ src/controller/TelaPrincipalController.java
@@ -1,3 +1,39 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class TelaPrincipalController {
+
+    @FXML
+    private void abrirCadastro(ActionEvent event) {
+        trocarTela("/view/cadastroTime.fxml");
+    }
+
+    @FXML
+    private void abrirPartidas(ActionEvent event) {
+        trocarTela("/view/partidas.fxml");
+    }
+
+    @FXML
+    private void abrirTabela(ActionEvent event) {
+        trocarTela("/view/tabela.fxml");
+    }
+
+    private void trocarTela(String caminho) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(caminho));
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
