d9bc7221f787b16a55718fb04b230cfb5328299e
victogr555 <victogr25@gmail.com>
2026-05-30 18:59:42 -0300

    arquivo cadastroTimeController concluido

--- src/controller/CadastroTimeController.java
+++ src/controller/CadastroTimeController.java
@@ -1,2 +1,48 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import model.Time;
+import util.Dados;
+
 public class CadastroTimeController {
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextField txtCapitao;
+
+    @FXML
+    private void cadastrar() {
+
+        String nome = txtNome.getText();
+        String capitao = txtCapitao.getText();
+
+        if(nome.isEmpty() || capitao.isEmpty()) {
+
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setContentText("Preencha todos os campos!");
+            alert.show();
+
+            return;
+        }
+
+        Time time = new Time(
+                nome,
+                "CS2",
+                capitao,
+                5
+        );
+
+        Dados.times.add(time);
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setContentText("Time cadastrado com sucesso!");
+        alert.show();
+
+        txtNome.clear();
+        txtCapitao.clear();
+    }
 }
