1ee736f7ab394877957e651cdba62aa58d9564e4
Gustavo Ribeiro <gustavoribeirom2003@gmail.com>
2026-06-08 12:33:06 -0300

    Update files

---
+++ src/main/java/com/campeonato/MainApp.java
@@ -0,0 +1,25 @@
+package com.campeonato;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("fxml/main_view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 840, 520);
+
+        stage.setTitle("E-Sports Championship CS2");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
